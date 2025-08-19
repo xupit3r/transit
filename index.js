305fa9f6e044aa972b6bc484ea62872c52b43cdd
Joe D'Alessandro <joe@thejoeshow.net>
2025-08-19 07:46:19 -0400

    generalized things and pulled in urban data

--- index.js
+++ index.js
@@ -2,17 +2,27 @@ const pdfParser = require('pdf-parser');
 
 const PDF_PATH = 'transit.pdf';
 
-const DESIRED_PAGE = 39;
+const URBAN_TRANSPORT_PAGES = [
+	38
+];
 
-const pageExtract = (page) => {
-	const START = "COMMUNITY TRANSPORTATION";
-	const END = "Community Transportation:";
+const COMMUNITY_TRANSPORT_PAGES = [
+	39
+];
 
+const extractPages = (pages, start, end) => pages.map(
+	page => extractPage(page, start, end)
+);
+const filterPages = (pdf, desired) => pdf.pages.filter(page => {
+	return desired.indexOf(page.pageId + 1) > -1;
+});
+
+const extractPage = (page, start, end) => {
 	const startIdx = page.texts.findIndex(entry => {
-		return entry.text === START;
+		return entry.text.trim() === start;
 	});
 	const endIdx = page.texts.findIndex(entry => {
-		return entry.text === END;
+		return entry.text.trim() === end;
 	}) + 2;
 	const relevant = page.texts.slice(startIdx, endIdx);
 	const text = relevant.filter(entry => {
@@ -36,10 +46,17 @@ pdfParser.pdf2json(PDF_PATH, (err, pdf) => {
 		return console.error(err);
 	}
 
-	const page = pdf.pages.find(page => {
-		return page.pageId === DESIRED_PAGE - 1;
-	});
+	const urbanResults = extractPages(
+		filterPages(pdf, URBAN_TRANSPORT_PAGES),
+		"URBAN SYSTEM",
+		"Last Base Fare Increase:"
+	);
 
-	// console.log(JSON.stringify(page, null, 2));
-	console.log(pageExtract(page));	
+	const communityResults = extractPages(
+		filterPages(pdf, COMMUNITY_TRANSPORT_PAGES),
+		"COMMUNITY TRANSPORTATION",
+		"Community Transportation:"
+	);
+
+
 });
